dc55b806d94b307888810cf574c43429fce45d87
Phillippe Rocha <phillipperocha@ROTTEIN.localdomain>
2020-11-22 17:17:35 -0300

    Creating first component

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,22 @@
+// Vamos importar como S para quando batermos o olho não surgir a dúvida se isso é estilo ou
+// Um componente de verdade. Quando tiver o S sabemos que é estilo
+import * as S from './styles'
+
 const Main = () => (
-  <main>
-    <h1>Advanced React</h1>
-  </main>
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Imagem de um átomo e React Avançado escrito ao lado"
+    />
+    <S.Title>Advanced React</S.Title>
+    <S.Description>
+      TypeScript, ReactJS, NextJS e Styled Components
+    </S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Um desenvolvedor de frente para uma tela com código"
+    />
+  </S.Wrapper>
 )
 
 export default Main
