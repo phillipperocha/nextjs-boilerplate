772269e72e71bb0d3358d3d0256caa1221ebf3c5
Phillippe Rocha <phillipperocha@ROTTEIN.localdomain>
2020-11-22 16:43:05 -0300

    Creating global style and global url setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,24 @@
-import '../../styles/globals.css'
+import GlobalStyles from 'styles/globals'
+import Head from 'next/head'
 
 import type { AppProps /*, AppContext */ } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Advanced React - Boilerplate</title>
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch icon" href="/img/icon-512.png" />
+        <meta
+          name="description"
+          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+        />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
-export default MyApp
+export default App
